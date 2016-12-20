df8753452290f544239c547809f2e71df76d67b0
jdimov59 <jdimov59@gmail.com>
2016-12-20 19:34:32 +0200

    Created New Solution
    1. Created Class Libraries - DGTempCF.Data & DGTempCF.DataModels
    2. Created ASP.NET Web Application -  DGTempCF.Web
    3. Updated all NuGet packages

---
+++ Source/DGTempCF/Web/DGTempCF.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DGTempCF.Web.Startup))]
+namespace DGTempCF.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
