2dee0b76328c46f7e6e15f42450483054e97b646
jdimov59 <jdimov59@gmail.com>
2016-12-21 14:32:10 +0200

    Test
    1. In DGTempCF.DataModels  added classes Student, Course and Enrollment
    2. In DGTempCF.Data added DGTempCFContext.cs and DGTempCFInitializer.cs
    3. Changed Web.Config file
    4. Created StudentController, CoursesController, EnrollmentsController
    and Views by scaffolding.
    28

---
+++ Source/DGTempCF/Data/DGTempCF.Data/DGTempCFContext.cs
@@ -0,0 +1,23 @@
+﻿using DGTempCF.DataModels;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace DGTempCF.Data
+{
+    public class DGTempCFContext : DbContext
+    {
+        public DGTempCFContext() : base("DGTempCF")
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+    }
+}
